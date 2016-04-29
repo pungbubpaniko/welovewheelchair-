dc5d98f3df871e176198845f6715fb1d4e800879
fang <difdif1995@gmail.com>
2016-04-29 10:07:23 +0700

    t

---
+++ app/src/main/java/appewtc/masterung/welovewheelchair/CommentWheelChair.java
@@ -0,0 +1,13 @@
+package appewtc.masterung.welovewheelchair;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CommentWheelChair extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment_wheel_chair);
+    }
+}
